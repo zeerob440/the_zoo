6290e0c24ebe7dd1f83854586919ae0475a334db
gabney <gabney@ivytech.edu>
2026-02-09 20:40:03 -0500

    Update models.py

--- zoo_entrance/models.py
+++ zoo_entrance/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    category = models.CharField(max_length=255)
+    subcategory = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    amount = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+pass
