1a76511dd02e73cb5994bc883ae9b6dcbdca04a5
zeerob440 <zeerob440@tutanota.com>
2026-02-14 12:43:22 -0500

    feat: zoo_entrance forms.py created. user login form created.

---
+++ zoo_entrance/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class userLoginForm(forms.Form):
+    username = forms.CharField(
+        max_length = 25,
+        label = 'Enter your name'
+    )
+
+    # given no password protected login for expediency and simplicity, logins will be written as lower-case only.
+    def clean_username(self):
+        username =self.cleaned_data['username']
+        return username.strip().lower()
