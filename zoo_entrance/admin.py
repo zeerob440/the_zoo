f1ee20ffd7041a570bbbe21bb7a8a766e60f617e
gabney <gabney@ivytech.edu>
2026-02-23 20:35:14 -0500

    Update admin.py

--- zoo_entrance/admin.py
+++ zoo_entrance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ZooUser
+from .models import Zoo
+from .models import ZooAnimal
 
 # Register your models here.
+admin.site.register(ZooUser)
+admin.site.register(Zoo)
+admin.site.register(ZooAnimal)
