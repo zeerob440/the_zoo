9e6adec66bc00f7d975a5206d2f7461e23346057
gabney <gabney@ivytech.edu>
2026-02-25 17:53:58 -0500

    Fix missing work from week6work
    Corrected missing models.py and migration while keeping updates to html templates

--- zoo_entrance/forms.py
+++ zoo_entrance/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 #creates form for user login at zoo_entrance
 
+# Form to allow users to log in
 class userLoginForm(forms.Form):
     username = forms.CharField(
         max_length = 25,
@@ -11,3 +12,31 @@ class userLoginForm(forms.Form):
     def clean_username(self):
         username =self.cleaned_data['username']
         return username.strip().lower()
+
+
+# Form to allow creating new zoos
+class ZooCreationForm(forms.Form):
+    zoo_name = forms.CharField(
+        max_length = 25,
+        label = 'Enter a name for the zoo'
+    )
+    # potential spot to add a boolean to control if zoo is public or not
+    # public = forms.BooleanField()
+
+
+# Form to create new animals
+class AnimalCreationForm(forms.Form):
+    # we will need to create a dropdown list for valid animals here instead of a character field
+    species = forms.CharField(
+        max_length = 25,
+        label = 'Enter the species'
+    )
+    nickname = forms.CharField(
+        max_length = 25,
+        label = 'Give your animal a name'
+    )
+    # we will need to create a dropdown for valid food here instead of a character field
+    food = forms.CharField(
+        max_length = 25,
+        label = 'Enter what food the animal will eat'
+    )
--- zoo_entrance/models.py
+++ zoo_entrance/models.py
@@ -1,10 +1,47 @@
 from django.db import models
 
-# Create your models here.
 
-#user model
+
+# USER MODEL
 class ZooUser(models.Model):
+    # ATTRIBUTES
     username = models.CharField(max_length = 25, unique = True)
+    # we are forgoing password for now to limit complexity
+    # password = models.CharField(max_length = 25)
 
+    # METHODS
     def __str__(self):
-        return self.username
+        return f"{self.username} is the current user."
+    
+    
+
+# ZOO MODEL
+class Zoo(models.Model):
+    # ATTRIBUTES
+    zoo_user = models.ForeignKey(ZooUser, on_delete=models.CASCADE)
+    zoo_name = models.CharField(max_length = 25, unique = True)
+    # potential attribute for making a public zoos page for everyone to see
+    # public = models.BooleanField()
+
+
+    # METHODS
+    def __str__(self):
+        return f"{self.zoo_name} is owned by {self.zoo_user}."
+    
+
+
+# ANIMAL MODEL
+class ZooAnimal(models.Model):
+    # ATTRIBUTES
+    zoo = models.ForeignKey(Zoo, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length = 25, unique = True)
+    species = models.CharField(max_length = 25)
+    food = models.CharField(max_length=50)
+
+
+    # METHODS
+    def __str__(self):
+        return f"{self.nickname} is a {self.species} in {self.zoo}."
+    
+    def feed(self):
+        pass
