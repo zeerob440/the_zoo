c99651106d39282b1b1e4e640919222f1a8ea2ad
zeerob440 <zeerob440@tutanota.com>
2026-02-28 13:31:08 -0500

    fixed: user name now passing in zoo_detail() and displaying in zoo_detail.html

--- zoo_entrance/views.py
+++ zoo_entrance/views.py
@@ -53,32 +53,53 @@ def zoo_home(request):
     animals = ZooAnimal.objects.filter(zoo__zoo_user=user)
     #animals = [1,2,3,4]
     # all values being passed must be in the SAME dictionary as different key value pairs
-    # pass user into zoo_home render, test to see if it returns the user's name in the <h2> element in home.html
+    # pass 'user' : user into zoo_home render, test to see if it returns the user's name in the <h2> element in home.html, it works. 
     return render(request, 'zoo_entrance/zoo_list.html', {'zoos': zoos,'animals':animals, 'user' : user})
     
     #return render(request, 'zoo_entrance/home.html', {'user': user})
 
 
 def zoo_detail(request, pk):
-    zoo = get_object_or_404(Zoo, pk=pk)
-    animals = ZooAnimal.objects.filter(pk=zoo.pk)
-    return render(request, 'zoo_entrance/zoo_detail.html', {'zoo': zoo, 'animals': animals})
+    #render user name in zoo_detail.html
+    user_id = request.session.get('user_id')
+    user = ZooUser.objects.get(id=user_id)
+    zoo = get_object_or_404(Zoo, pk=pk, zoo_user=user)
+    animals = ZooAnimal.objects.filter(zoo=zoo)
+    return render(request, 'zoo_entrance/zoo_detail.html', {'zoo': zoo, 'animals': animals, 'user': user})
 
 
 def zoo_new(request):
-    if request.method == "POST":
-        form = ZooForm(request.POST)
-        if form.is_valid():
-            zoo = form.save(commit=False)
+     #if request.method == "POST":
+        #form = ZooForm(request.POST)
+        #if form.is_valid():
+            #zoo = form.save(commit=False)
             # use the following line when we get login working and then remove the zoo.zoo_user=testuser line
             #zoo.zoo_user = request.user
-            zoo.zoo_user = ZooUser.objects.get(username='testuser')
-            zoo.save()
-            return redirect('zoo_entrance:zoo_detail', pk=zoo.pk)
+            #zoo.zoo_user = ZooUser.objects.get(username='testuser')
+            #zoo.save()
+            #return redirect('zoo_entrance:zoo_detail', pk=zoo.pk)
+    #else:
+        #form = ZooForm()
+    #return render(request, 'zoo_entrance/zoo_edit.html', {'form': form})
+   # connect a new zoo to current user
+    user_id = request.session.get('user_id')
+
+    if not user_id:
+       return redirect('zoo_entrance:index')
+   
+    user = ZooUser.objects.get(id=user_id)
+
+    if request.method == 'POST':
+       form = ZooForm(request.POST)
+       if form.is_valid():
+           zoo = form.save(commit=False)
+           # set new zoo to logged in user
+           zoo.zoo_user = user
+           zoo.save()
+           return redirect('zoo_entrance:zoo_detail', pk=zoo.pk) 
     else:
         form = ZooForm()
-    return render(request, 'zoo_entrance/zoo_edit.html', {'form': form})
-
+    return render(request, 'zoo_entrance/zoo_edit.html', {'form' : form})
 
 def zoo_edit(request, pk):
     zoo = get_object_or_404(Zoo, pk=pk)
@@ -94,8 +115,11 @@ def zoo_edit(request, pk):
 
 
 def animal_detail(request, pk):
+    # add username to detail.html
+    user_id = request.session.get('user_id')
+    user = ZooUser.objects.get(id=user_id)
     animal = get_object_or_404(ZooAnimal, pk=pk)
-    return render(request, 'zoo_entrance/animal_detail.html', {'animal': animal})
+    return render(request, 'zoo_entrance/animal_detail.html', {'animal': animal, 'user' : user})
 
 
 def animal_new(request, pk):
